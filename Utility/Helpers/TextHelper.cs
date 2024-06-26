e656254bd3d9c6c108cee9623fbafd0a51ae486b
Thabang Teddy Morwasetla <teddymorwasetla@gmail.com>
2024-06-26 22:36:27 +0200

    AD | added Text helper

---
+++ Utility/Helpers/TextHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Utility.helpers
+{
+    public class TextHelper
+    {
+
+        // Method to minimize the length of a string using all strategies
+        public static string MinimizeLength(string? input, int maxLength)
+        {
+            if (string.IsNullOrEmpty(input) || maxLength <= 0)
+                return string.Empty;
+
+            return input.Length <= maxLength ? input : input.Substring(0, maxLength) + "...";
+        }
+    }
+}
